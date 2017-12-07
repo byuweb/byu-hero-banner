8fc97c26d9099d332dee0fa0a29cd5f0716c16b0
mkpratt <mkpratt6@gmail.com>
2017-12-06 17:03:35 -0700

    Updated video option to have a placeholder image while the video loads. Image fades out once video starts playing. Minimized black screen between video loops. Video rescale needs some more love on mobile landscape orientation.

--- byu-hero-banner/byu-hero-banner.js
+++ byu-hero-banner/byu-hero-banner.js
@@ -33,10 +33,9 @@ class ByuHeroBanner extends HTMLElement {
     util.applyTemplate(this, 'byu-hero-banner', template, () => {
       setupSlotListeners(this);
       applyImageSource(this);
-      applyvideoID(this);
+      applyVideoID(this);
     });
   }
-
 
   static get observedAttributes() {
     return [];
@@ -48,7 +47,7 @@ class ByuHeroBanner extends HTMLElement {
         applyImageSource(this);
         break;
       case ATTR_VIDEO_ID:
-        applyvideoID(this);
+        applyVideoID(this);
         break;
     }
   }
@@ -74,8 +73,6 @@ class ByuHeroBanner extends HTMLElement {
     }
     return '';
   }
-
-
 }
 
 window.customElements.define('byu-hero-banner', ByuHeroBanner);
@@ -93,10 +90,9 @@ function applyImageSource(component) {
   hiddenImage.src = component.imageSource;
 }
 
-function applyvideoID(component) {
+function applyVideoID(component) {
 
   if (component.videoID && component.videoID != '') {
-
 
     let videoBox = component.shadowRoot.querySelector('#player');
     //videoBox.src = component.videoID;
@@ -108,47 +104,98 @@ function applyvideoID(component) {
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     // Create the player object when API is ready
-
+    var player;
     window.onYouTubeIframeAPIReady = function () {
-      let player = new YT.Player(videoBox, {
+      player = new YT.Player(videoBox, {
+        width: '100%',
+        height: '100%',
         videoId: component.videoID,
         playerVars: {
-          autoplay: 0, 
+          autoplay: 1, 
           autohide: 1,
-          modestbranding: 0,
+          modestbranding: 1,
+          playsinline: 1,
           rel: 0, 
           showinfo: 0, 
           controls: 0,
           disablekb: 1,
           enablejsapi: 0,
-          iv_load_policy: 3
+          iv_load_policy: 3,
+          fs: 0
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onStateChange
         }
       });
     };
 
-    component.shadowRoot.querySelector('#player').style.display = 'block';
-    window.addEventListener('load', vidRescale);
+    function onPlayerReady(event) {
+      // Limits the amount of black screen before restarting the video
+      setInterval(function() {
+        var i = (player.getCurrentTime()/player.getDuration())*100;
+        // if the video is at least 99% complete, restart the video
+        if (i >= 99) {
+          player.seekTo(0);
+        }
+      }, 25);
+      player.mute();
+      vidRescale();
+      // This forces it to start playing on mobile devices
+      player.playVideo();
+    }
+    
+    function onStateChange(event) {
+      switch (event.data) {
+        case YT.PlayerState.PLAYING:
+          // Fade out the starting image
+          component.shadowRoot.querySelector('#hidden-image').classList.add('hidden');
+          break;
+        case YT.PlayerState.ENDED:
+          // Use this instead of a looped playlist to prevent loading the video again 
+          // causing a longer black screen in between
+          player.playVideo(); 
+          break;
+      }
+    }
+
+    let iframe = component.shadowRoot.querySelector('#player');
+    iframe.style.display = 'inline';
+    iframe.style.marginLeft = 0;
+    
+    // window.addEventListener('load', vidRescale);
     window.addEventListener('resize', vidRescale);
+
+    function vidRescale() {
+      let w = window.innerWidth, h = window.innerHeight;
+      //console.log(w);
+      if (w/h > 16/9) {
+        player.setSize(w, w/(16/9));
+      } else {
+        player.setSize(h*(16/9), h);
+      }
+    }
   }
 }
 
-function vidRescale(){
+// function vidRescale(){
+//   let w = window.innerWidth + 200, h = window.innerHeight + 200;
+//   let els = document.querySelectorAll(".tv, .screen");
+//   if (w/h > 16/9){
+//     tv.setSize(w, w/16*9);
+//     for (let i = 0; i < els.length; i++) {
+//       els[i].style.left = '0px';
+//     }
+//     //$('.tv .screen').css({'left': '0px'});
+//   } else {
+//     tv.setSize(h/9*16, h);
+//     $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
+//   }
+// }
 
-  var w = $(window).width()+200,
-    h = $(window).height()+200;
-
-  if (w/h > 16/9){
-    tv.setSize(w, w/16*9);
-    $('.tv .screen').css({'left': '0px'});
-  } else {
-    tv.setSize(h/9*16, h);
-    $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
-  }
-}
-
-$(window).on('load resize', function(){
-  vidRescale();
-});
+// $(window).on('load resize', function(){
+//   vidRescale();
+// });
 
 function setupSlotListeners(component) {
   // let slot = component.shadowRoot.querySelector('#fancy-template');
